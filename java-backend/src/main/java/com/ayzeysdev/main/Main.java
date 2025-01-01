61fa33db67139042d36387af568bd1065358f457
Aravind Kumar <ara3194@gmail.com>
2025-01-01 21:56:13 +1100

    Test success, git push verify

--- java-backend/src/main/java/com/ayzeysdev/main/Main.java
+++ java-backend/src/main/java/com/ayzeysdev/main/Main.java
@@ -3,6 +3,5 @@ package com.ayzeysdev.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Movie Recommendation System is running!");
-        System.out.println("Created git access token - TEST");
     }
 }
