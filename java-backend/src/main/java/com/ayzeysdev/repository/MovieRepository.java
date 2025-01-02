0acc4a0649400a03ab87ab5aebcc7d887130b762
Aravind Kumar <ara3194@gmail.com>
2025-01-02 21:30:02 +1100

    SLF4J working pom

--- java-backend/src/main/java/com/ayzeysdev/repository/MovieRepository.java
+++ java-backend/src/main/java/com/ayzeysdev/repository/MovieRepository.java
@@ -1,6 +1,14 @@
 package com.ayzeysdev.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MovieRepository {
+    // Corrected method name (getLogger with uppercase 'L')
+    private static final Logger logger = LoggerFactory.getLogger(MovieRepository.class);
 
-
+    public void logTest() {
+        logger.info("Logging setup is successful!");
+    }
 }
+
