956b3ceaa2b361e774318e84304e580222f01662
Aravind Kumar <ara3194@gmail.com>
2025-01-03 20:53:11 +1100

    Refactor before adding next steps

--- java-backend/src/main/java/com/ayzeysdev/main/Main.java
+++ java-backend/src/main/java/com/ayzeysdev/main/Main.java
@@ -1,11 +1,9 @@
 package com.ayzeysdev.main;
-
 import com.ayzeysdev.repository.MovieRepository;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Movie Recommendation System is running!");
         MovieRepository repository = new MovieRepository();
-        repository.logTest(); // Should print: Logging setup is successful!
     }
 }
--- java-backend/src/main/java/com/ayzeysdev/repository/MovieRepository.java
+++ java-backend/src/main/java/com/ayzeysdev/repository/MovieRepository.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MovieRepository {
-    // Corrected method name (getLogger with uppercase 'L')
     private static final Logger logger = LoggerFactory.getLogger(MovieRepository.class);
 
     public void logTest() {
